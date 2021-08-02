047c7981272c234e9426a67bc036420fbf49420a
Galatia <ptrivedius@aol.com>
2021-08-02 17:13:48 -0400

    worked on JavaScript functions

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var apiKey = '64049e14167e493d01838b9c3f5dcbb0';
-var apiCurrentUrl;
+var apiUrl = "https://openweathermap.org";
 var apiFutureUrl;
 var apiUltraViolet;
 var searchInput = document.getElementById("search-input");
+var searchButton = document.getElementById("search-button");
 
 var historySearches = document.getElementById("history");
 
@@ -11,12 +12,16 @@ function showSavedLocations() {
     if (locations) {
         var parsedLocations = JSON.parse(locations);
         parsedLocations.forEach(function(item) {
-            var listItem = document.createElement('li');
-            var content = `<button data-location="${item}">${item}</button>`
-            listItem.innerHTML = content;
-            historySearches.appendChild(listItem);
+            createHistoryButton(item);
         });
     }
+}
+
+function createHistoryButton(location) {
+    var listItem = document.createElement('li');
+    var content = `<button data-location="${location}">${location}</button>`
+    listItem.innerHTML = content;
+    historySearches.appendChild(listItem);
 }
 
 function updateContentPane(event) {
@@ -24,8 +29,54 @@ function updateContentPane(event) {
     var location = buttonClicked.getAttribute("data-location");
 }
 
+function searchCity(event) {
+    event.preventDefault()
+    var location = searchInput.value;
+    if (location) {
+        window.alert("Please enter a location!");
+    }
+
+    var url = `$(apiUrl)/data/2.5/find?q=${location}&appid=${appid}`
+
+    fetch(url).then(function(response) {
+        if (!response.ok) {
+            window.alert('fetch failed');
+        }
+        return response.json()
+
+    }).then(function(data) {
+        if (data.count === 0) {
+            window.alert("This is not a valid location!");
+        }
+        doSuccessfulFetch(data, location);
+    })
+}
+
 function setEventListeners() {
-    historySearches.addEventListener('click', updateContentPane)
+    historySearches.addEventListener('click', updateContentPane);
+    searchInput.addEventListener("click", searchCity)
+}
+
+function setLocalStorage(location) {
+    var locations = localStorage.getItem("savedLocations");
+    var parsedLocations = [];
+
+    if (locations) {
+        parsedLocations = JSON.parse(locations);
+    }
+    var hasLocation = parsedLocations.some(function (loc){
+        return loc.toLowerCase() === location.toLowerCase();
+    });
+
+    if (!hasLocation) {
+        parsedLocations.push(location);
+        localStorage.setItem("savedLocations", JSON.stringify(parsedLocations));
+    }
+}
+
+function doSuccessfulFetch(data, location) {
+    createHistoryButton(location);
+    setLocalStorage(location)
 }
 
 function init() {
