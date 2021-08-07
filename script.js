27ef4c7e331520f3dfc5b3d22c1ef8497f5724e5
Galatia <ptrivedius@aol.com>
2021-08-06 21:59:48 -0400

    pushing current code

--- script.js
+++ script.js
@@ -43,9 +43,10 @@ function updateContentPane(event) {
 
 // var city = document.getElementById("search-input").innerText;
 
-var myLocation = searchInput.value;
+
 function searchCity(event) {
-    event.preventDefault();
+    event.preventDefault()
+    var myLocation = searchInput.value;
     if (myLocation === "") {
         window.alert("Please enter a location!");
     }
